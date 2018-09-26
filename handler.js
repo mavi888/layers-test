04c33b33b6c24341f41a73258fbb0af45984c792
Marcia Villalba <marciavillalba@Marcias-MacBook-Pro.local>
2018-09-26 14:15:26 +0300

    Middy middleware

--- handler.js
+++ handler.js
@@ -1,12 +1,26 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      secretValue: process.env.SECRET_VALUE
-    })
-  };
+const middy = require('middy');
+const { ssm } = require('middy/middlewares');
 
-  callback(null, response);
+const originalHandler = (event, context, callback) => {
+	const response = {
+		statusCode: 200,
+		body: JSON.stringify({
+			secretValue: context.value1
+		})
+	};
+
+	callback(null, response);
 };
+
+module.exports.hello = middy(originalHandler).use(
+	ssm({
+		cache: true,
+		cacheExpiryInMillis: 1 * 60 * 1000, // 1 mins
+		setToContext: true,
+		names: {
+			value1: '/middy-test/value1'
+		}
+	})
+);
